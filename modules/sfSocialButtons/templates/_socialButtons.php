97a0311fc6568322f6430fdd9d9094148294c9d4
Andre Salmon <andre.salmon@braindata.de>
2014-07-01 12:11:16 +0200

    CHANGE: Update to Bootstrap 3 and changes for responsive view // ADD: Use the bootstrap-social framework

--- modules/sfSocialButtons/templates/_socialButtons.php
+++ modules/sfSocialButtons/templates/_socialButtons.php
@@ -1,13 +1,23 @@
-<span class="hidden-phone">
-    <?php use_stylesheet('/sfSocialButtonsPlugin/css/socialButtons') ?>
-    <a rel="popup" class="btn btn-sm socialBtn google" target="_blank" href="https://plus.google.com/share?url=<?php echo $url ?>">
-        <i class="socialBtn-icon-googleplus"></i><?php echo __('gplus') ?> (<?php echo $plus; ?>)
+<span class="hidden-xs">
+    <a rel="popup" class="btn btn-sm btn-social btn-google-plus" target="_blank" href="https://plus.google.com/share?url=<?php echo $url ?>">
+        <i class="fa fa-google"></i><?php echo __('gplus') ?> (<?php echo $plus; ?>)
     </a>
-    <a rel="popup" class="btn btn-sm socialBtn facebook" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $url ?>&t=<?= $item ?>">
-        <i class="socialBtn-icon-facebook"></i><?php echo __('facebook') ?> (<?php echo $likes; ?>)
+    <a rel="popup" class="btn btn-sm btn-social btn-facebook" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $url ?>&t=<?= $item ?>">
+        <i class="fa fa-facebook"></i><?php echo __('facebook') ?> (<?php echo $likes; ?>)
     </a>
-     <a rel="popup" class="btn btn-sm socialBtn twitter" target="_blank" href="https://twitter.com/share?url=<?php echo $url ?>">
-        <i class="socialBtn-icon-twitter"></i><?php echo __('twitter') ?> (<?php echo $tweets; ?>)
+    <a rel="popup" class="btn btn-sm btn-social btn-twitter" target="_blank" href="https://twitter.com/share?url=<?php echo $url ?>">
+        <i class="fa fa-twitter"></i><?php echo __('twitter') ?> (<?php echo $tweets; ?>)
+    </a>
+</span>
+<span class="visible-xs">
+    <a rel="popup" class="btn btn-xs btn-social btn-google-plus" target="_blank" href="https://plus.google.com/share?url=<?php echo $url ?>">
+        <i class="fa fa-google"></i><?php echo __('gplus') ?> (<?php echo $plus; ?>)
+    </a>
+    <a rel="popup" class="btn btn-xs btn-social btn-facebook" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $url ?>&t=<?= $item ?>">
+        <i class="fa fa-facebook"></i><?php echo __('facebook') ?> (<?php echo $likes; ?>)
+    </a>
+    <a rel="popup" class="btn btn-xs btn-social btn-twitter" target="_blank" href="https://twitter.com/share?url=<?php echo $url ?>">
+        <i class="fa fa-twitter"></i><?php echo __('twitter') ?> (<?php echo $tweets; ?>)
     </a>
 </span>
 
